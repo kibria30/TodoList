43720a6c46862c09304bbb05fa7eb7e81edf2b9c
kibria30 <kibria8007@gmail.com>
2024-09-20 23:19:19 +0600

    Updateview is complete

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
-from .views import TaskListView, TaskDetailView, TaskCreateView
+from .views import TaskListView, TaskDetailView, TaskCreateView, TaskUpdateView
 
 urlpatterns = [
     # path('', views.Home, name="Task-Home"),
     path('', TaskListView.as_view(), name="task-home"),
-    path('task/', TaskListView.as_view(), name="task-home"),
+    # path('task/', TaskListView.as_view(), name="task-home"),
     path('task/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
+    path('task/<int:pk>/update/', TaskUpdateView.as_view(), name='task-update'),
     path('task/new/', TaskCreateView.as_view(), name="task-create")
     # path('task/new/', views.taskFormView, name="TaskForm")
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Task
 from .forms import TaskModelForm
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.urls import reverse_lazy
 
 # def Home(request):
@@ -24,37 +24,25 @@ class TaskCreateView(CreateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('task-home')
+    
+class TaskUpdateView(UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy('task-home')
 
-def task(request):
-    return HttpResponse('Enter a new task.')
+# def task(request):
+#     return HttpResponse('Enter a new task.')
 
-def taskFormView(request):
+# def taskFormView(request):
     
     
-    if request.method == "POST":
-        form = TaskModelForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("/")
-    else:
-        form = TaskModelForm()
-        context = {'form' : form}
+#     if request.method == "POST":
+#         form = TaskModelForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("/")
+#     else:
+#         form = TaskModelForm()
+#         context = {'form' : form}
 
-    return render(request, 'tasks/taskForm.html', context)
-
-
-
-def change_state(request, ):
-    if request.method == "POST":
-        # Get the record ID from the POST data
-        record_id = request.POST.get('record_id')
-        
-        # Fetch the record from the database
-        record = get_object_or_404(Task, id=record_id)
-        
-        # Modify the record's state (or any other field)
-        record.complete = True  # Change state to the desired value
-        record.save()  # Save the changes to the database
-        
-        # Redirect or render a template after updating the record
-        return redirect('home')  # Redirect to home page or another view
+#     return render(request, 'tasks/taskForm.html', context)
